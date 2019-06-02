79e9c6d581c20d741cb8146c4e5418b25744d0be
Ifeoluwa <bolaji.olawoye@hostvillenigeria.org>
2019-06-02 19:12:28 +0100

    Setup

--- src/routes/web.php
+++ src/routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
-Route::group(['namespace' => 'Dorcas\ModulesServiceRequests\Http\Controllers', 'middleware' => ['web']], function() {
-    Route::get('sales', 'ModulesServiceRequestsController@index')->name('sales');
+Route::group(['namespace' => 'Dorcas\ModulesServiceRequests\Http\Controllers', 'middleware' => ['web','auth']], function() {
+    Route::get('service-requests-main', 'ModulesServiceRequestsController@index')->name('service-requests-main');
 });
 
 
